887e826383a0e96a8856c241975b42d3e3cafc6e
José Sánchez-Gallego <gallegoj@uw.edu>
2021-04-30 19:02:09 -0700

    Add CLI

---
+++ src/simple_com_server/__main__.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2021-04-30
+# @Filename: __main__.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+import asyncio
+
+import click
+from sdsstools.daemonizer import DaemonGroup, cli_coro
+
+from simple_com_server.server import TCPServer
+
+
+@click.group(cls=DaemonGroup, prog="com-server")
+@click.option(
+    "--device",
+    multiple=True,
+    type=str,
+    required=True,
+    help="Path to the serial device. Can be called multiple times for several devices.",
+)
+@click.option(
+    "--port",
+    multiple=True,
+    type=int,
+    required=True,
+    help="Port on which to serve. Can be called multiple times for several devices.",
+)
+@click.option(
+    "--timeout",
+    type=float,
+    default=1.0,
+    help="Time to wait for a reply from the serial device.",
+)
+@cli_coro()
+async def com_server(device, port, timeout):
+    """Start a TCP-to-COM server."""
+
+    assert len(device) == len(port), "Number of devices must match number of ports."
+    assert len(set(port)) == len(port), "Ports must be unique"
+
+    servers = await asyncio.gather(
+        *[
+            TCPServer(
+                str(device[ii]),
+                port[ii],
+                timeout=timeout,
+            ).start()
+            for ii in range(len(port))
+        ]
+    )
+
+    await servers[0].server.serve_forever()
